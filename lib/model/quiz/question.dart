645a454badec49c2906260d5048fad14d674be0d
Zoey Child <imperialoctopus@gmail.com>
2021-01-09 18:38:36 +0000

    Update question model

--- lib/model/quiz/question.dart
+++ lib/model/quiz/question.dart
@@ -1,13 +1,22 @@
-import 'answer.dart';
-
 /// Question for a quiz.
 class Question {
-  /// Text of question.
-  final String question;
+  /// Image path.
+  final String imagePath;
 
   /// List of potential answers.
-  final List<Answer> answers;
+  final Map<String, bool> answers;
+
+  /// Size of dog.
+  final String size;
+
+  /// Rarity of dog.
+  final String rarity;
 
   /// Const constructor.
-  const Question({required this.question, required this.answers});
+  const Question({
+    required this.imagePath,
+    required this.answers,
+    required this.size,
+    required this.rarity,
+  });
 }
