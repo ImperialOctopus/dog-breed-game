1bc2d6928e4cecee6e0bef839a3e3c71ec27473d
Zoey Child <imperialoctopus@gmail.com>
2021-03-23 15:37:30 +0000

    Add world model

--- lib/model/world.dart
+++ lib/model/world.dart
@@ -1,3 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'level/level.dart';
+
+/// Data structure for a world.
 class World {
-  final Widget 
+  /// Widget to display as title.
+  final Widget title;
+
+  /// Icon image.
+  final Image iconImage;
+
+  /// List of levels.
+  final List<Level> levels;
+
+  /// Data structure for a world.
+  const World({
+    required this.title,
+    required this.iconImage,
+    required this.levels,
+  });
 }
