72be54dbc2106cfc45666d1efe305bc0fecf2bb4
Zoey Child <imperialoctopus@gmail.com>
2021-03-23 16:53:46 +0000

    Add empty widgets to routes

---
+++ lib/routes/learn/learn_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Learn main route.
+class LearnRoute extends StatelessWidget {
+  /// Learn main route.
+  const LearnRoute();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/routes/learn/level_route.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../model/level/level.dart';
+
+/// Route for a level.
+class LevelRoute extends StatelessWidget {
+  /// Level to display.
+  final Level level;
+
+  /// Route for a level.
+  const LevelRoute({required this.level});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
