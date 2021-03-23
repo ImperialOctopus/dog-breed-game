6a77c2724b4318e02fcc3b4a392d0c29e6a48cfc
Zoey Child <imperialoctopus@gmail.com>
2021-03-23 16:53:46 +0000

    Add empty widgets to routes

---
+++ lib/routes/learn/world_route.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../model/world.dart';
+
+/// Level select inside a world.
+class WorldRoute extends StatelessWidget {
+  /// World to display.
+  final World world;
+
+  /// Level select inside a world.
+  const WorldRoute({required this.world});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/routes/menu/menu_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Main menu route.
+class MenuRoute extends StatelessWidget {
+  /// Main menu route.
+  const MenuRoute();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
