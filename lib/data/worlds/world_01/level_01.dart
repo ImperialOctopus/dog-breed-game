f090bb8f42cc4ab5637853923c50b8214648000c
Zoey Child <imperialoctopus@gmail.com>
2021-03-23 17:23:26 +0000

    Create sample world

---
+++ lib/data/worlds/world_01/level_01.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../../../component/level/lesson.dart';
+import '../../../component/level/level.dart';
+import '../../../model/quiz/question.dart';
+import '../../../model/quiz/quiz.dart';
+
+/// Level 1 - Common breeds
+const level_01 = Level(
+  id: 0,
+  title: 'Level 1',
+  subtitle: 'Common Breeds',
+  iconData: Icons.ac_unit,
+  imagePath: 'assets/sample/labrador.jpg',
+  description:
+      'First level. Check out some information about the top 5 most common dogs in the UK and then try out the quiz to see how well you can recognise them!',
+  lesson: Lesson(lessonBody: [
+    Text('Less-on-1'),
+  ]),
+  quiz: Quiz(questions: [
+    Question(
+      imagePath: 'assets/sample/labrador.jpg',
+      answers: [
+        'Dog',
+        'Cat',
+        'Labrador',
+        'Pug',
+      ],
+      correctAnswer: 2,
+      size: 'Small',
+      rarity: 'Unobtainable',
+    ),
+    Question(
+      imagePath: 'assets/sample/box.jpg',
+      answers: [
+        'Conglomerate',
+        'Zebra',
+        'Pog',
+        'Pug',
+      ],
+      correctAnswer: 1,
+      size: 'Small',
+    ),
+    Question(
+      imagePath: 'assets/sample/kiss.jpg',
+      answers: [
+        'Doge',
+        'Cat',
+        'Box',
+        'Resplendent Quetzal',
+      ],
+      correctAnswer: 3,
+      size: 'Very Big',
+    ),
+    Question(
+      imagePath: 'assets/sample/look.jpg',
+      answers: [
+        'SpongeBob',
+        'Spongebob',
+        'Spong Bobe',
+        'Bob-Sponge',
+      ],
+      correctAnswer: 0,
+      size: 'Sponge',
+      rarity: 'Bob',
+    ),
+  ]),
+);
---
+++ lib/data/worlds/world_01/level_02.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../../component/level/level.dart';
+
+/// Level 2 - Uncommon breeds
+const level_02 = Level(
+  id: 1,
+  title: 'Level 2',
+  subtitle: 'Uncommon Breeds',
+  iconData: Icons.send_to_mobile,
+  imagePath: 'assets/sample/box.jpg',
+  description:
+      'Second level! Now we\'re going to try out some less common breeds. Read through the summary under \'Lesson\', then try the Quiz and see how you do.',
+  lesson: null,
+  quiz: null,
+);
