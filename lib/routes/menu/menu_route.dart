abff92fef83b5ca76fdb91a11430b56d6cb8332e
Zoey Child <imperialoctopus@gmail.com>
2021-03-24 17:00:21 +0000

    Add provides page abstract class for routes

--- lib/routes/menu/menu_route.dart
+++ lib/routes/menu/menu_route.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 import '../../screen/menu/menu_screen.dart';
+import '../provides_page.dart';
 
 /// Main menu route.
-class MenuRoute extends StatelessWidget {
+class MenuRoute extends StatelessWidget with ProvidesPage<void> {
   /// Main menu route.
   const MenuRoute();
 
@@ -12,13 +13,27 @@ class MenuRoute extends StatelessWidget {
     return const MenuScreen();
   }
 
-  Page get page => MaterialPage<void>(child: this);
+  @override
+  Page<void> get page => _MenuRoutePage(route: this);
 }
 
 class _MenuRoutePage extends Page<void> {
+  final MenuRoute route;
+
+  const _MenuRoutePage({required this.route});
+
   @override
   Route createRoute(BuildContext context) {
-    // TODO: implement createRoute
-    throw UnimplementedError();
+    return PageRouteBuilder<void>(
+      settings: this,
+      pageBuilder: (context, animation, animation2) {
+        final tween = Tween(begin: const Offset(0.0, 1.0), end: Offset.zero);
+        final curveTween = CurveTween(curve: Curves.easeInOut);
+        return SlideTransition(
+          position: animation.drive(curveTween).drive(tween),
+          child: route,
+        );
+      },
+    );
   }
 }
