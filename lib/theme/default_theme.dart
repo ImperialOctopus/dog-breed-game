e3deea2b2f5590e4959fd015b52ac8a13b4c8d03
Zoey Child <imperialoctopus@gmail.com>
2021-11-27 16:43:40 +0000

    Update themes to new flutter version

--- lib/theme/default_theme.dart
+++ lib/theme/default_theme.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
-/// App's default theme.
-class DefaultTheme {
-  /// Primary colour.
-  static const primaryColor = Color.fromARGB(255, 110, 20, 69);
-
-  /// Accent colour.
-  static const accentColor = Color.fromARGB(255, 181, 51, 70);
-
-  /// Text theme.
-  static const textTheme = TextTheme(
+/// Default theme for this app.
+final defaultTheme = ThemeData.from(
+  colorScheme: const ColorScheme.light(
+    primary: Color.fromARGB(255, 110, 20, 69),
+    //primaryVariant:  Color(0xff1c313a),
+    secondary: Color.fromARGB(255, 181, 51, 70),
+    //secondaryVariant: Color(0xffc79400),
+  ),
+  textTheme: const TextTheme(
     // Main app title
     headline1: TextStyle(
       fontSize: 48,
@@ -28,24 +27,28 @@ class DefaultTheme {
       fontWeight: FontWeight.normal,
       //color: Colors.black54,
     ),
-  );
-
-  /// Outlined button theme.
-  static final outlinedButtonTheme = OutlinedButton.styleFrom(
-    textStyle: const TextStyle(fontSize: 32),
-    minimumSize: const Size(200, 60),
-  );
-
-  /// Elevated button theme.
-  static final elevatedButtonTheme = ElevatedButton.styleFrom(
-    textStyle: const TextStyle(fontSize: 20),
-    minimumSize: const Size(200, 60),
-  );
-
-  /// Text button theme.
-  static final textButtonTheme = TextButton.styleFrom(
-    primary: const Color.fromARGB(255, 127, 127, 127),
-    textStyle: const TextStyle(fontSize: 20),
-    minimumSize: const Size(200, 60),
-  );
-}
+  ),
+).copyWith(
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 32),
+      minimumSize: const Size(200, 60),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+      minimumSize: const Size(200, 60),
+    ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      primary: const Color.fromARGB(255, 127, 127, 127),
+      textStyle: const TextStyle(fontSize: 20),
+      minimumSize: const Size(200, 60),
+    ),
+  ),
+  textSelectionTheme: const TextSelectionThemeData(
+    cursorColor: Color.fromARGB(255, 181, 51, 70),
+  ),
+);
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,5 +1,21 @@
-import 'dark_theme.dart';
-import 'light_theme.dart';
+import 'package:flutter/material.dart';
+
+import 'default_theme.dart';
+
+/// Light version of the theme.
+final lightTheme = defaultTheme.copyWith(
+  brightness: Brightness.light,
+);
+
+/// Dark version of the theme.
+final darkTheme = defaultTheme.copyWith(
+  brightness: Brightness.dark,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: defaultTheme.elevatedButtonTheme.style?.merge(
+      ElevatedButton.styleFrom(onPrimary: Colors.white),
+    ),
+  ),
+);
 
 /// Default app theme
 final themeData = darkTheme;
