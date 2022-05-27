f739812df04762dae1cdf1087f9079755850a442
Zoey Child <imperialoctopus@gmail.com>
2022-05-27 15:55:03 +0100

    Add link to results page at end of quiz

--- lib/theme/animation.dart
+++ lib/theme/animation.dart
@@ -2,4 +2,7 @@
 class AnimationTheme {
   /// Duration for slide switchers in the quiz view.
   static const quizSwitcherDuration = Duration(milliseconds: 300);
+
+  /// Duration for progress slide moving.
+  static const progressSlideDuration = Duration(milliseconds: 100);
 }
