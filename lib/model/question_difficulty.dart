9786231f5d145bf20675147bf51d2667c58e86e9
Zoey Child <imperialoctopus@gmail.com>
2022-06-08 23:06:50 +0100

    Change question difficulty to class enum

--- lib/model/question_difficulty.dart
+++ lib/model/question_difficulty.dart
@@ -1,14 +1,38 @@
 /// Difficulty category of a question.
-enum QuestionDifficulty {
-  /// Level 1: Beginner
-  beginner,
+class QuestionDifficulty {
+  /// Enum index.
+  final int index;
 
-  /// Level 2: Intermediate
-  intermediate,
+  /// Text name of this difficulty.
+  final String name;
 
-  /// Level 3: Expert
-  expert,
+  const QuestionDifficulty._(this.index, this.name);
 
-  /// Level 4: Challenge
-  challenge,
+  /// Create enum from index.
+  factory QuestionDifficulty.fromIndex(int _index) => _all[_index];
+
+  @override
+  String toString() => name;
+
+  static const List<QuestionDifficulty> _all = [
+    beginner,
+    intermediate,
+    expert,
+    challenge,
+  ];
+
+  /// Beginner difficulty (1/4)
+  static const QuestionDifficulty beginner =
+      QuestionDifficulty._(0, 'Beginner');
+
+  /// Intermediate difficulty (2/4)
+  static const QuestionDifficulty intermediate =
+      QuestionDifficulty._(1, 'Intermediate');
+
+  /// Expert difficulty (3/4)
+  static const QuestionDifficulty expert = QuestionDifficulty._(2, 'Expert');
+
+  /// Challenge difficulty (4/4)
+  static const QuestionDifficulty challenge =
+      QuestionDifficulty._(3, 'Challenge');
 }
