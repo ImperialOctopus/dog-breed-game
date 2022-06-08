2ada3d60a5e3430b1e1ef12ae6c03810a8014af9
Zoey Child <imperialoctopus@gmail.com>
2022-06-08 23:06:50 +0100

    Change question difficulty to class enum

--- lib/screens/practice/components/settings_info_bar.dart
+++ lib/screens/practice/components/settings_info_bar.dart
@@ -26,65 +26,65 @@ class SettingsInfoBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: [
-          Expanded(
-              child: Column(
-            children: [
-              const Padding(
-                  padding: EdgeInsets.all(5),
-                  child: FaIcon(FontAwesomeIcons.temperatureHigh)),
-              Text(difficulty.toString()),
-            ],
-          )),
-          Expanded(
-            child: (timeEnabled)
-                ? Column(
-                    children: [
-                      const Padding(
-                          padding: EdgeInsets.all(5),
-                          child: Icon(Icons.timer_outlined)),
-                      Expanded(
-                        child: Text('a'),
-                      ),
-                    ],
-                  )
-                : Column(
-                    children: [
-                      const Padding(
-                          padding: EdgeInsets.all(5),
-                          child: Icon(Icons.timer_off_outlined)),
-                    ],
-                  ),
-          ),
-          Expanded(
-            child: (lives == null)
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Padding(
-                          padding: EdgeInsets.all(5),
-                          child: FaIcon(FontAwesomeIcons.heartCircleXmark)),
-                      Text('$mistakes'),
-                    ],
-                  )
-                : Column(
-                    children: [
-                      (mistakes < lives!)
-                          ? const Padding(
-                              padding: EdgeInsets.all(5),
-                              child: FaIcon(FontAwesomeIcons.heart),
-                            )
-                          : const Padding(
-                              padding: EdgeInsets.all(5),
-                              child: FaIcon(FontAwesomeIcons.heartCrack)),
-                      Text('${lives! - mistakes} / $lives'),
-                    ],
-                  ),
-          ),
-        ],
+    return SizedBox(
+      height: 150,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+              child: (timeEnabled)
+                  ? Column(
+                      children: [
+                        const Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Icon(Icons.timer_outlined)),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        const Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Icon(Icons.timer_off_outlined)),
+                      ],
+                    ),
+            ),
+            Expanded(
+                child: Column(
+              children: [
+                const Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FaIcon(FontAwesomeIcons.temperatureHigh)),
+                Text(difficulty.toString()),
+              ],
+            )),
+            Expanded(
+              child: (lives == null)
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Padding(
+                            padding: EdgeInsets.all(5),
+                            child: FaIcon(FontAwesomeIcons.heartCircleXmark)),
+                        Text('$mistakes'),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        (mistakes < lives!)
+                            ? const Padding(
+                                padding: EdgeInsets.all(5),
+                                child: FaIcon(FontAwesomeIcons.heart),
+                              )
+                            : const Padding(
+                                padding: EdgeInsets.all(5),
+                                child: FaIcon(FontAwesomeIcons.heartCrack)),
+                        Text('${lives! - mistakes} / $lives'),
+                      ],
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
